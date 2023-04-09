e02e892241274040191fb36e739de7b448c911b3
Нуржан <nurzhan.k@lightsoul.dev>
2023-04-09 08:07:51 +0600

    feat: AoPanel color

--- src/mainscript.lua
+++ src/mainscript.lua
@@ -17,7 +17,7 @@ local IsAOPanelEnabled = GetConfig("EnableAOPanel") or GetConfig("EnableAOPanel"
 
 local function onAOPanelStart(p)
 	if IsAOPanelEnabled then
-		local SetVal = { val = userMods.ToWString("DD") }
+		local SetVal = { val1 = userMods.ToWString("DD"), class1 = "RelicCursed" }
 		local params = { header = SetVal, ptype = "button", size = 32 }
 		userMods.SendEvent("AOPANEL_SEND_ADDON",
 			{ name = common.GetAddonName(), sysName = common.GetAddonName(), param = params })
